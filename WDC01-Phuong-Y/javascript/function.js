e25b1c62359143ea7292ee2bcd84b68c83befc1f
ytvn <=>
2019-07-19 00:16:56 +0700

    ver2

--- WDC01-Phuong-Y/javascript/function.js
+++ WDC01-Phuong-Y/javascript/function.js
@@ -17,21 +17,26 @@ function setImage(){
         
         //  document.getElementById(i.toString()).style.backgroundImage = "url('../resource/avatar.jpg')";
        // document.getElementById(i.toString()).style.backgroundImage = "url('avatar.jpg')";
-       //document.getElementById(i.toString()).style.backgroundImage ='url('+'avatar.jpg'+')';
+      // document.getElementById(i.toString()).style.backgroundImage ='url('+'avatar.jpg'+')';
       //  document.body.style.backgroundImage = "url('avatar.jpg')";
-        document.getElementById(i.toString()).style.backgroundColor="red";
+       // var x='../resource/'+source[i];
+
+        document.getElementById(i.toString()).style.backgroundImage= "url('resource/a1.jpg')";
+       // document.getElementsByClassName("bg-img")[0].style.backgroundImage='url('+x+')';
+       // document.getElementById(i.toString()).style.backgroundColor="red";
       
     }
 }
 
 function LoadImg(){
-    var img=['a1.jpg',"a2.jpg",'b1.jpg',"b2.jpg",'c1.jpg',"c2.jpg",'d1.jpg',"d2.jpg",'e1.jpg',"e2.jpg",'f1.jpg',"f2.jpg",'g1.jpg',"g2.jpg",'h1.jpg',"h2.jpg"];
+    var img=['a1.jpg',"a2.jpg",'b1.jpg',"b2.jpg"];//,'c1.jpg',"c2.jpg",'d1.jpg',"d2.jpg",'e1.jpg',"e2.jpg",'f1.jpg',"f2.jpg",'g1.jpg',"g2.jpg",'h1.jpg',"h2.jpg"];
 
-    shuffle(img);
+    //shuffle(img);
 
-    for(var i=0;i<=15;i++){
-        document.getElementById(i.toString()).style.backgroundImage= 'url('+img[i].toString+')';
-    }
+    for(var i=0;i<=3;i++){
+         document.getElementById(i.toString()).style.backgroundImage= 'url(resource/'+img[i]+')';
+       
+    } 
 
 
 }
